ed7a94d67662f1e421add1da4a7fc28fb88e219c
Oleh-Mashovets <mashovets700@gmail.com>
2023-01-04 21:52:07 +0200

    first comment

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let inputText = document.getElementById('getText');
+let getInput = document.getElementById('result');
+let btnReset = document.getElementById('button__reset');
+let btnEnter = document.getElementById('button__enter');
+let showText = document.querySelector('.entry');
+
+btnReset.addEventListener('click', () => {
+    getInput.innerHTML = inputText.value = '';
+})
+btnEnter.addEventListener('click', () => {
+    getInput.innerHTML = inputText.value;
+})
+
+btnEnter.addEventListener('click', () => {
+    if (!inputText.value.length) {
+        showText.classList.add('is-visible');
+    }
+    else {
+        showText.classList.remove('is-visible');
+    }
+})
